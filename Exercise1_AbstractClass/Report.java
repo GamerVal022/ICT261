e925e98b92ba89692b0d4ad8ffbd60cf53a3bc59
kuundabanda <savys794@gmail.com>
2026-09-22 02:00:31 +0200

    "My  exercise 1 abstract"

---
+++ Exercise1_AbstractClass/Report.java
@@ -0,0 +1,23 @@
+abstract class Report {
+    protected String title;
+
+    public Report(String title) {
+        this.title = title;
+    }
+
+    void printTitle() {
+        System.out.println(title);
+    }
+
+    abstract void generate();
+}
+
+class SalesReport extends Report {
+    SalesReport() {
+        super("Sales");
+    }
+
+    void generate() {
+        System.out.println("Generating sales report");
+    }
+}
